7b7e10cf564f8a84b46bc11789f8710af9b33274
Taylor Hodge <j.taylor.hodge@gmail.com>
2019-12-24 23:03:48 -0500

    replace createElement function with object

--- didact.js
+++ didact.js
@@ -3,11 +3,19 @@
 // 1. replace the code inside tags with a call to createElement
 // 2. pass the tag name, props, and children as params to createElement
 // const element = <h1 title="top-header">Hello world</h1>;
-const element = React.createElement(
-  "h1",
-  { title: "top-header" },
-  "Hello world"
-);
+// const element = React.createElement(
+//   "h1",
+//   { title: "top-header" },
+//   "Hello world"
+// );
+// createElement builds an object (and performs validations) from the given params
+const element = {
+  type: "h1",
+  props: {
+    title: "top-header",
+    children: "Hello world"
+  }
+};
 // grab a node from the DOM
 const container = document.getElementById("root");
 // render the React element in the container
