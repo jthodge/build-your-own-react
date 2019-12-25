14f2d68cead43980a2e77421b28f20b400fc6cbf
Taylor Hodge <j.taylor.hodge@gmail.com>
2019-12-24 22:59:48 -0500

    repalce JSX element with vanilla JS

--- didact.js
+++ didact.js
@@ -2,7 +2,12 @@
 // JSX → JS via built tools e.g. Babel
 // 1. replace the code inside tags with a call to createElement
 // 2. pass the tag name, props, and children as params to createElement
-const element = <h1 title="top-header">Hello world</h1>;
+// const element = <h1 title="top-header">Hello world</h1>;
+const element = React.createElement(
+  "h1",
+  { title: "top-header" },
+  "Hello world"
+);
 // grab a node from the DOM
 const container = document.getElementById("root");
 // render the React element in the container
