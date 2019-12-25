3e3681b2c296274ca1fd8a038fb9b8ba82515e34
Taylor Hodge <j.taylor.hodge@gmail.com>
2019-12-24 23:55:05 -0500

    add comments

--- didact.js
+++ didact.js
@@ -1,13 +1,15 @@
 // define a React element using JSX
+// const element = <h1 title="top-header">Hello world</h1>;
+
 // JSX → JS via built tools e.g. Babel
 // 1. replace the code inside tags with a call to createElement
 // 2. pass the tag name, props, and children as params to createElement
-// const element = <h1 title="top-header">Hello world</h1>;
 // const element = React.createElement(
 //   "h1",
 //   { title: "top-header" },
 //   "Hello world"
 // );
+
 // createElement builds an object (and performs validations) from the given params
 const element = {
   type: "h1",
@@ -16,6 +18,12 @@ const element = {
     children: "Hello world"
   }
 };
+// an element is an object with two properties: type and props
+// (more: https://github.com/facebook/react/blob/f4cc45ce962adc9f307690e1d5cfa28a288418eb/packages/react/src/ReactElement.js#L111)
+// type: string specifying type of DOM node to create; tagName param passed to document.createElemtn
+// props: object containing key-value pairs for JSX attributes; contains special property children
+// children: string || array
+
 // grab a node from the DOM
 const container = document.getElementById("root");
 // render the React element in the container
