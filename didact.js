da5a1119682d68afc201ad9f181b6e34e1530cd0
Taylor Hodge <j.taylor.hodge@gmail.com>
2019-12-24 22:58:17 -0500

    add comments

--- didact.js
+++ didact.js
@@ -1,3 +1,9 @@
+// define a React element using JSX
+// JSX → JS via built tools e.g. Babel
+// 1. replace the code inside tags with a call to createElement
+// 2. pass the tag name, props, and children as params to createElement
 const element = <h1 title="top-header">Hello world</h1>;
+// grab a node from the DOM
 const container = document.getElementById("root");
+// render the React element in the container
 ReactDOM.render(element, container);
